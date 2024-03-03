1eaa546a9b2d647e3333e06f91082b4f8d2d7a52
Fjolla2002 <fjolla.selimi17@gmail.com>
2024-03-03 23:23:58 +0100

    added navbar component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router } from 'react-router-dom';
 import AppRoutes from "./routes/appRoutes";
 import "./styles/global.scss";
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
 
   return (
     <Router>
+      <Navbar />
       <AppRoutes />
     </Router>
   )
--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,8 +1,46 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleUser } from '@fortawesome/free-regular-svg-icons';
+import "../../styles/navbar.scss";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className="navbar">
+      <div className="container">
+        <div className="nav-content">
+          <div className="nav-col right">
+            <NavLink to="/" className="logo">
+              Kosova Hike
+            </NavLink>
+          </div>
+          <div className="nav-col">
+            <ul className="nav-links">
+              <li className="nav-link">
+                <NavLink>
+                  Explore
+                </NavLink>
+              </li>
+              <li className="nav-link">
+                <NavLink>
+                  About
+                </NavLink>
+              </li>
+              <li className="nav-link">
+                <NavLink>
+                  Contact Us
+                </NavLink>
+              </li>
+            </ul>
+          </div>
+          <div className="nav-col">
+            <NavLink className="account">
+              <FontAwesomeIcon icon={faCircleUser} /> Account
+            </NavLink>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
